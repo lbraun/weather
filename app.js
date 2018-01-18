f36edcab6d99d7783e279edf9fc995736d2069f6
Lucas Braun <lucasalan@gmail.com>
2018-01-18 10:59:26 +0100

    Build out station details

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ $(document).on("click", "#refresh-button", function () {
                     // Populate list with fresh stations
                     $.each(stations, function (index, station) {
                         $('#station-list').append(
-                            '<li><a id="station-details-link" href="#">' + station.name +
+                            '<li data-id=' + index + '><a id="station-details-link" href="#">' + station.name +
                             '<span class="ui-li-count">' + Math.round(station.main.temp) + 'º</span>' +
                             '</a></li>');
                     });
@@ -41,20 +41,24 @@ $(document).on("click", "#refresh-button", function () {
         })
 });
 
-$(document).on('pageBeforeShow', '#index-page', function () {
+$(document).on('pagebeforeshow', '#index-page', function () {
     $(document).on('click', '#station-details-link', function (e) {
         e.preventDefault();
         e.stopImmediatePropagation();
-
         // Store the id of the station that was clicked
-        CURRENT_STATION = stations[e.target.children[0].id];
+        CURRENT_STATION = stations[e.target.parentElement.dataset["id"]];
 
         // Navigate to details page
         $.mobile.changePage("#station-details-page");
     })
 });
 
-$(document).on('pageBeforeShow', '#station-details-page', function (e) {
+$(document).on('pagebeforeshow', '#station-details-page', function (e) {
     e.preventDefault();
-    $('#stationName').text(CURRENT_STATION.name);
+    $('#station-icon').attr('src','http://openweathermap.org/img/w/' + CURRENT_STATION.weather[0].icon + '.png');
+    $('#station-name').text(CURRENT_STATION.name);
+    $('#station-description').text(CURRENT_STATION.weather[0].description);
+    $('#station-temp').text('Temperature: ' + CURRENT_STATION.main.temp);
+    $('#station-humidity').text('Humidity: ' + CURRENT_STATION.main.humidity + ' %');
+    $('#station-pressure').text('Pressure: ' + CURRENT_STATION.main.pressure + ' hpa');
 });
